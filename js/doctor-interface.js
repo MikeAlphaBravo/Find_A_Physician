0ecada3948ebe29d5959b0b647ac964602af050c
Michael Brooks <mikeyabrooks@gmail.com>
2017-10-20 13:02:53 -0700

    add error message text output for api error and no doctors returned error

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -5,11 +5,9 @@ $(document).ready(function() {
   $("#form").submit(function() {
     event.preventDefault();
     $('#output').text('');
-    let name = $('#name').val();
-    let query = $('#query').val();
+    let nameInput = $('#name').val();
+    let queryInput = $('#query').val();
     let doctor = new Doctor();
-    let nameInput = `name=${name}`;
-    let queryInput = `&query=${query}`;
     // debugger;
     doctor.apiCall(nameInput, queryInput, apiKey);
   });
